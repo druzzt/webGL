e7699d793ab92ff8ee17bf22c9279e4165e57d88
druzzt <arek.lewand@gmail.com>
2016-04-24 16:01:05 +0200

    ani

--- ani.js
+++ ani.js
@@ -160,7 +160,7 @@ var checkboundaries = function(){
     //console.log(hitx4);
     var hity4t= data.object4.position[5]-(data.object1.position[1]+data.object1.radius);
     var hity4l= data.object4.position[7]-(data.object1.position[1]+data.object1.radius);
-    console.log("x:{"+hit4x+"};  ytl:{"+hity4t+", "+hity4l+"}");
+    console.log("x:{"+hitx4+"};  ytl:{"+hity4t+", "+hity4l+"}");
     // punkty
     if(data.object1.position[0]-data.object1.radius+0.015 <= -1){
         data.object1.direction[0]= -data.object1.direction[0];
