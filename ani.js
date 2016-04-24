828d89f9919794ea657dc431c4e5fcef7d5ff0c5
druzzt <arek.lewand@gmail.com>
2016-04-24 10:23:03 +0200

    ani

--- ani.js
+++ ani.js
@@ -166,7 +166,7 @@ var checkboundaries = function(){
     }
     
     
-    if(((data.object1.position[0]-data.object1.radius) <= data.object4.position[0]) && (((data.object1.position-data.object1.radius)<=data.object4.position[3])||((data.object1.position+data.object1.radius)>=data.object4.position[1]))){
+    if(((data.object1.position[0]-data.object1.radius) <= (data.object4.position[0]+data.object1.radius)) && (((data.object1.position-data.object1.radius)<=(data.object4.position[3]data.object1.radius))||((data.object1.position+data.object1.radius)>=(data.object4.position[1]data.object1.radius)))){
         console.log("dotknalem paletki 1");
         data.object1.direction[0]= -data.object1.direction[0];
     }
